99fae71525feebe8d5b9536d8ae65f6a3cc15b8a
Emmanuelle Sojka <emmanuelle.sojka@free.fr>
2024-02-28 08:27:29 +0100

    Added feature to filter Pokemon list by name

--- app/src/main/java/fr/imt/pokedex/PokedexListActivity.kt
+++ app/src/main/java/fr/imt/pokedex/PokedexListActivity.kt
@@ -4,13 +4,15 @@ import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import fr.imt.pokedex.adapter.PokemonListAdapter
 import fr.imt.pokedex.data.Datasource
 import fr.imt.pokedex.model.Pokemon
 import java.io.IOException
-
 const val TAG: String = "PokedexListActivity"
 class PokedexListActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +23,24 @@ class PokedexListActivity: AppCompatActivity() {
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
         val dataset: List<Pokemon> = Datasource().loadPokemons()
-        recyclerView.adapter = PokemonListAdapter(this, dataset)
+        val editText = findViewById<EditText>(R.id.pokedex_list_view_filter_text)
+        val imageButton =
+            findViewById<ImageButton>(R.id.pokedex_list_view_filter_clear)
+        imageButton.setOnClickListener { editText.setText("") }
+        editText.doOnTextChanged {searchText, _, _, _ ->
+            if (searchText != null) {
+                refreshList(recyclerView,
+                    dataset.filter { pokemon -> pokemon.name.contains(searchText,
+                        true) })
+            } else {
+                refreshList(recyclerView, dataset)
+            }
+        }
+        refreshList(recyclerView, dataset)
+    }
+    private fun refreshList(recyclerView: RecyclerView, filteredDataSet:
+    List<Pokemon>) {
+        recyclerView.adapter = PokemonListAdapter(this, filteredDataSet)
     }
     companion object {
         fun getAssetsDrawable(context: Context, fileName: String): BitmapDrawable? {
